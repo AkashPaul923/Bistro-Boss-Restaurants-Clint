e6739165c27b340a354a309a94822c5d6c80a88d
AkashPaul923 <akashpaul923@gmail.com>
2024-12-31 01:43:59 +0600

    review section add

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -6,23 +6,47 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-import './styles.css';
-
 // import required modules
-import { Navigation } from 'swiper/modules';
+import {Autoplay, Navigation } from 'swiper/modules';
+import SectionTitle from "./SectionTitle";
+
+import { Rating } from '@smastrom/react-rating'
+
+import '@smastrom/react-rating/style.css'
 
 
 const Review = () => {
-    const [review, setReview] = useState([])
+    const [reviews, setReviews] = useState([])
 
     useEffect(()=>{
         fetch('reviews.json')
         .then(res => res.json())
-        .then(data => setReview(data))
+        .then(data => setReviews(data))
     },[])
 
     return (
-        <div>
+        <div className="max-w-7xl mx-auto my-20">
+            <SectionTitle title={'TESTIMONIALS'} subTitle={'What Our Clients Say'}></SectionTitle>
+            <div>
+                <Swiper navigation={true} autoplay={{ delay: 3500, disableOnInteraction: false,}} loop={true} modules={[Autoplay,Navigation]} className="mySwiper">
+                    {
+                        reviews.map(review => <SwiperSlide key={review._id}>
+                            <div className="flex flex-col items-center justify-center text-center gap-5 py-10 px-6 lg:px-20">
+                                <div>
+                                    <Rating
+                                        style={{ maxWidth: 180 }}
+                                        value={review.rating}
+                                        readOnly
+                                    />
+                                </div>
+                                <div><img src="https://img.icons8.com/?size=100&id=NQjqeRm9PFRS&format=png&color=000000" alt="" /></div>
+                                <p>{review.details}</p>
+                                <p className="uppercase text-3xl text-yellow-600 font-medium">{review.name}</p>
+                            </div>
+                            </SwiperSlide>)
+                    }
+                </Swiper>
+            </div>
             
         </div>
     );
--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../Components/Banner";
 import Categories from "../Components/Categories";
 import Featured from "../Components/Featured";
 import PopularMenu from "../Components/PopularMenu";
+import Review from "../Components/Review";
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <Categories></Categories>
             <PopularMenu></PopularMenu>
             <Featured></Featured>
+            <Review></Review>
         </div>
     );
 };
