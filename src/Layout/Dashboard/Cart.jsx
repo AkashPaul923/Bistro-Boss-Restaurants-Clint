494789141591bbc66b267b1f4a47601713011f9c
AkashPaul923 <akashpaul923@gmail.com>
2025-01-16 00:55:07 +0600

    cart data delete perform

--- src/Layout/Dashboard/Cart.jsx
+++ src/Layout/Dashboard/Cart.jsx
@@ -1,10 +1,42 @@
 import { FaTrashAlt } from "react-icons/fa";
 import useCart from "../../Hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 
 const Cart = () => {
-    const [carts] = useCart()
+    const [carts, refetch] = useCart()
     const totalPrice = carts.reduce( (total, cart) => total+ cart.price, 0)
+    const axiosSecure = useAxiosSecure()
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/carts/${id}`)
+                .then(res =>{
+                    if(res.data.deletedCount){
+                        refetch()
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success"
+                        });
+                    }
+                })
+
+            }
+        });
+    }
+
+
     return (
         <div>
             <div className="flex justify-between items-center md:text-xl font-medium md:font-bold mb-10">
@@ -38,7 +70,7 @@ const Cart = () => {
                             <td>{cart.name}</td>
                             <td>${cart.price}</td>
                             <th>
-                              <button className="btn btn-ghost"><FaTrashAlt/></button>
+                              <button onClick={()=>handleDelete(cart._id)} className="btn btn-ghost"><FaTrashAlt/></button>
                             </th>
                         </tr>)
                         }
