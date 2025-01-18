e2bcb5bded64be0a81dc827f8da4320fba6d2436
AkashPaul923 <akashpaul923@gmail.com>
2025-01-18 14:06:43 +0600

    payment information saved in db

--- src/Layout/Dashboard/User/CheckoutForm.jsx
+++ src/Layout/Dashboard/User/CheckoutForm.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import useCart from "../../../Hooks/useCart";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import useAuth from "../../../Hooks/useAuth";
+import Swal from "sweetalert2";
 
 
 const CheckoutForm = () => {
@@ -11,17 +12,19 @@ const CheckoutForm = () => {
     const [clientSecret, setClientSecret] = useState("")
     const stripe = useStripe();
     const elements = useElements();
-    const [carts] = useCart()
+    const [carts, refetch] = useCart()
     const totalPrice = carts.reduce( (total, cart) => total+ cart.price, 0)
     const axiosSecure = useAxiosSecure()
     const [transactionId, setTransactionId] = useState('')
 
     useEffect(()=>{
-        axiosSecure.post('/create-payment-intent', {price : totalPrice})
-        .then(res => {
-            console.log(res.data.clientSecret)
-            setClientSecret(res.data.clientSecret)
-        })
+        if(totalPrice >0 ){
+            axiosSecure.post('/create-payment-intent', {price : totalPrice})
+            .then(res => {
+                console.log(res.data.clientSecret)
+                setClientSecret(res.data.clientSecret)
+            })
+        }
 
     },[axiosSecure, totalPrice])
 
@@ -70,6 +73,29 @@ const CheckoutForm = () => {
             console.log("payment intent", paymentIntent);
             if(paymentIntent.status === "succeeded"){
                 setTransactionId(paymentIntent.id)
+
+                const payment = {
+                    email: user.email,
+                    transactionId: paymentIntent.id,
+                    price: totalPrice,
+                    date: new Date(),
+                    cartIds : carts.map(item => item._id),
+                    menuItemIds : carts.map(item => item.menuId),
+                    status: "Pending"
+                }
+
+                const res = await axiosSecure.post('/payments', payment)
+                console.log("payment saved",res.data);
+                if(res?.data?.paymentResult?.insertedId){
+                    refetch()
+                    Swal.fire({
+                        // position: "top-end",
+                        icon: "success",
+                        title: "Successfully payment done",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
             }
         }
 
